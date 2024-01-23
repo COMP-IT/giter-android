f05805413548f3ffd5fc73790ef1baec8e118b1a
최민재 <cmjcmj1230@gmail.com>
2024-01-23 13:32:25 +0900

    ✅ :: add :core:domain module

---
+++ core/domain/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("giter.android.library")
+}
+
+android {
+    namespace = "com.comp.giter.core.domain"
+}
+
+dependencies {
+    implementation(projects.core.data)
+    // implementation(projects.core.model)
+
+    implementation(libs.inject)
+}
