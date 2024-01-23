9029910c87c3bbea1fcf97424941551f0e93f16e
최민재 <cmjcmj1230@gmail.com>
2024-01-23 12:59:08 +0900

    ✅ :: add :core:data module

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("giter.android.library")
+    id("giter.android.hilt")
+    id("kotlinx-serialization")
+}
+
+android {
+    namespace = "com.comp.giter.core.data"
+}
+
+dependencies {
+    // implementation(projects.core.model)
+    // implementation(projects.core.datastore)
+
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.datetime)
+    testImplementation(libs.turbine)
+}
